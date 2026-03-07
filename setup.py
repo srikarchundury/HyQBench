a8714bd18eda5a8176eee7dfb73160d6715a60af
Srikar Chundury <schundu3@ncsu.edu>
2026-03-07 15:12:38 -0500

    made hyqbench pip installable

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="hyqbench",
+	url="https://github.com/srikarchundury/HyQBench",
+	packages=find_packages(),
+	python_requires=">=3.11",
+)
